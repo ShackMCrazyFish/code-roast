f7b6e3088a230ca3fa0fe51b6cccecbad407b6fc
victor-drykin <VVDrykin@greenatom.ru>
2026-03-20 18:30:27 +0300

    app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
-import { RoastsModule } from './roasts/roasts.module';
-import { CommentsModule } from './comments/comments.module';
-import { TagsModule } from './tags/tags.module';
-import { SearchModule } from './search/search.module';
-import { FeedModule } from './feed/feed.module';
-import { NotificationModule } from './notification/notification.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { RoastsModule } from './modules/roasts/roasts.module';
+import { CommentsModule } from './modules/comments/comments.module';
+import { TagsModule } from './modules/tags/tags.module';
+import { SearchModule } from './modules/search/search.module';
+import { FeedModule } from './modules/feed/feed.module';
+import { NotificationModule } from './modules/notification/notification.module';
 
 @Module({
   imports: [
