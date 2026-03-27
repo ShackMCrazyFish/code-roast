786153136f5f9eba6bb5958527338a2dbaa125ab
victor-drykin <VVDrykin@greenatom.ru>
2026-03-27 18:23:51 +0300

    Create post module with CRUD routes

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { PostsController } from './posts.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Module({})
+@Module({
+  controllers: [PostsController],
+  providers: [PostsService, PrismaService],
+})
 export class PostsModule {}
